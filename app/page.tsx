3caeef717c43c52953d66ea28c2ec51d52d6e8ec
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-05 10:55:17 +0100

    add dynamic routing to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import { Button } from '@/components/ui/button';
+import { auth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
   return (
     <div className="w-screen h-screen flex justify-center items-center gap">
       <div className="flex flex-col gap-3 w-full max-w-[600px] mx-auto">
         <h1 className="text-7xl">Devjournal</h1>
         <p className="text-2xl text-foreground/45">Track your dev learning</p>
-        <Link href="/journal">
+        <Link href={href}>
           <Button className="w-[20%]">get started</Button>
         </Link>
       </div>
