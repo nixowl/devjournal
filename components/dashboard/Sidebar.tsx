10228adf4d730ab2523c79b25e7b76234f7245a7
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-05 10:57:48 +0100

    add sidebar component

---
+++ components/dashboard/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <aside className="h-full w-[20%] border-r border-black/20">
+            Mood            
+        </aside>
+    );
+}
+
+export default Sidebar;
