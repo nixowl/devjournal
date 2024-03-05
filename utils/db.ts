0a267e03f6d1e5b52f81ae42bd9423ce7a7a1941
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-05 10:59:05 +0100

    modify db.ts to initialize PrismaClient and add logging

---
+++ utils/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Check if prisma is in "this" global space, and if it's not, make it and assign it to a variable called prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma;
