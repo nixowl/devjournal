74116fb23cf8f3fc6986143ef41a68d50103d516
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-05 10:57:41 +0100

    add header component

---
+++ components/dashboard/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="border-b border-black/10 h-[10%]">
+            hello from the header
+        </header>
+    );
+}
+
+export default Header;
