baf57a66e3ffdf41fdfbe8cecc1c677f30d74a11
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-06 16:31:28 +0100

    some styling on sidebar

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
     return (
-        <aside className="h-full w-[20%] border-r border-black/20">
+        <aside className="h-full w-[14%] min-w-48 border-r border-black/20 bg-red-500">
             Mood            
         </aside>
     );
