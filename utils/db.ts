ea74398437e5e519de29e069f7a1f06c919e4cf9
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-06 16:35:09 +0100

    clarify comment

--- utils/db.ts
+++ utils/db.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// Check if prisma is in "this" global space, and if it's not, make it and assign it to a variable called prisma
+// Check if prisma is in "this" global space, and if it's not, make it and assign it to a variable called prisma. Every query will be logged to the console.
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
