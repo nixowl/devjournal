bc70c3e96bba59ad926a00a519882c8dfe25bfe2
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-06 16:30:47 +0100

    add PATCH routes for journal entries

---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,22 @@
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+import { NextResponse } from "next/server"
+
+//TODO: ts errors
+export const PATCH = async (request, { params }) => {
+    const { content } = await request.json()
+    const user = await getUserByClerkId({})
+    const updatedEntry = await prisma.journalEntry.update({
+        where: {
+            userId_id: {
+                userId: user.id,
+                id: params.id
+            }
+        },
+        data: {
+            content,
+        }
+    })
+
+    return NextResponse.json({ data: updatedEntry })
+}
