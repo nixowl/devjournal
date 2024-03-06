469098d609426c74b53d7497f8e2878090abe451
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-06 16:31:17 +0100

    add header with clerk user button

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Header = () => {
     return (
-        <header className="border-b border-black/10 h-[10%]">
-            hello from the header
+        <header className="border-b border-black/10 h-16">
+            <div className="h-full w-full px-6 flex items-center justify-end"><UserButton /></div>
         </header>
     );
 }
