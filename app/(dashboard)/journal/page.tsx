27f514577ca6bd9e8884220153d4d549960375d1
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-06 16:29:59 +0100

    add journal page with logic to create new entries and display entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,42 @@
-const JournalPage = () => {
-  return <div>Journal</div>;
+import EntryCard from '@/components/entries/EntryCard';
+import NewEntryCard from '@/components/entries/NewEntryCard';
+import { getUserByClerkId } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+import Link from 'next/link';
+
+// Requires logic for getting the authorized user in the prisma database based on the clerkId, helper function in auth.ts
+// TODO: Fix 'property id does not exist on type void'
+const getEntries = async () => {
+  const user = await getUserByClerkId({});
+
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return entries;
+};
+
+const JournalPage = async () => {
+  const entries = await getEntries();
+
+  return (
+    <div className="p-10 bg-gray-400/30 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard entry={entry} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default JournalPage;
