29a06fa275b474bb7213e88c413cea17fb69a4db
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-10 12:43:14 +0100

    move ai sidebar to editor component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,15 +1,75 @@
 'use client';
 
-import { useState } from "react";
+import { updateEntry } from '@/utils/api';
+import { useState } from 'react';
+import { useAutosave } from 'react-autosave';
 
-// TODO: ts error
+type editorProps = {
+  entry: {
+    id: string;
+    content: string;
+    analysis: {
+      title: string;
+      summary: string;
+      topic: string;
+      color: string;
+    };
+  };
+};
 
-const Editor = ({ entry }) => {
-    const [content, setContent] = useState(entry.content)
+const Editor = ({ entry }: editorProps) => {
+  const [content, setContent] = useState(entry.content);
+  const [isLoading, setIsLoading] = useState(false);
+  const [analysis, setAnalysis] = useState(entry.analysis);
 
-    return <div className='w-full h-full'>
-        <textarea className='w-full h-full p-8 text-xl' value={content} onChange={(e) => setContent(e.target.value)} />
-  </div>;
+  const { title, summary, topic, color } = analysis || entry.analysis;
+
+  const analysisData = [
+    { name: 'Topics', value: topic },
+    { name: 'Summary', value: summary },
+  ];
+
+  useAutosave({
+    data: content,
+    onSave: async (_content) => {
+      if (_content === entry.content) return;
+      setIsLoading(true);
+      const data = await updateEntry(entry.id, _content);
+      setAnalysis(data.analysis);
+      setIsLoading(false);
+    },
+  });
+
+  return (
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        {isLoading && <div>...loading</div>}
+        <textarea
+          className="w-full h-full p-8 text-xl"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </div>
+      <div className="border-l border-gray/35">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">{title}</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between px-4 py-2 border-b border-gray/35"
+              >
+                <span className="text-md font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
