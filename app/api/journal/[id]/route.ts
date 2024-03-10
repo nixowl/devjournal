52c34c619e2f424ca9b410d36767ddb078837077
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-10 12:42:47 +0100

    add logic to update journal entry and analysis

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,22 +1,37 @@
-import { getUserByClerkId } from "@/utils/auth"
-import { prisma } from "@/utils/db"
-import { NextResponse } from "next/server"
+import { analyze } from '@/utils/ai';
+import { getUserByClerkId } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+import { revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
 
 //TODO: ts errors
 export const PATCH = async (request, { params }) => {
-    const { content } = await request.json()
-    const user = await getUserByClerkId({})
-    const updatedEntry = await prisma.journalEntry.update({
-        where: {
-            userId_id: {
-                userId: user.id,
-                id: params.id
-            }
-        },
-        data: {
-            content,
-        }
-    })
+  const { content } = await request.json();
+  const user = await getUserByClerkId({});
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content,
+    },
+  });
 
-    return NextResponse.json({ data: updatedEntry })
-}
+    // TODO: ts errors
+  const analysis = await analyze(updatedEntry.content);
+  const updated = await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    update: analysis,
+  });
+  
+    return NextResponse.json({ data: { ...updatedEntry, analysis: updated } });
+};
