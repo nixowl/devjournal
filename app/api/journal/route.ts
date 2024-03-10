2d34a49f38b56adee5222ad22773b5ba8cf42dc4
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-10 12:42:16 +0100

    add logic to create a new analysis for a new journal entry

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai';
 import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 import { revalidatePath } from 'next/cache';
@@ -12,8 +13,21 @@ export const POST = async () => {
     },
   });
 
-    // Whenever a page gets data with something like fetch, its gonna be cached and subsequent requests can be served with the cached data. To make sure new data is rendered, we can revalidate the path on every request
-    revalidatePath('/journal')
-    
-    return NextResponse.json({ data: entry });
+  const analysis = await analyze(entry.content);
+  if (analysis) {
+    await prisma.analysis.create({
+      data: {
+        entryId: entry.id,
+        title: analysis.title || 'Default title',
+        summary: analysis.summary || '',
+        topic: analysis.topic || '',
+        color: analysis.color || '',
+      },
+    });
+  }
+
+  // Whenever a page gets data with something like fetch, its gonna be cached and subsequent requests can be served with the cached data. To make sure new data is rendered, we can revalidate the path on every request
+  revalidatePath('/journal');
+
+  return NextResponse.json({ data: entry });
 };
