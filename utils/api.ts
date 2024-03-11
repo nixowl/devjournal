5dce454cfb1c45c712d2710a656a4423837d1781
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-11 14:17:07 +0100

    feat: add POST request to db for questions

--- utils/api.ts
+++ utils/api.ts
@@ -3,26 +3,42 @@ const createURL = (path: string) => window.location.origin + path;
 export const createNewEntry = async () => {
   const res = await fetch(
     new Request(createURL('/api/journal'), {
-       method: 'POST',
-     })
-  )
-  
+      method: 'POST',
+    })
+  );
+
   if (res.ok) {
-    const data = await res.json()
-    return data.data
+    const data = await res.json();
+    return data.data;
   }
 };
 
-export const updateEntry = async (id, content) => {
+export const updateEntry = async (id: string, content: string) => {
   const res = await fetch(
     new Request(createURL(`/api/journal/${id}`), {
       method: 'PATCH',
-      body: JSON.stringify({ content })
+      body: JSON.stringify({ content }),
     })
-  )
+  );
 
   if (res.ok) {
-    const data = await res.json()
-    return data.data
+    const data = await res.json();
+    return data.data;
   }
-}
+};
+
+export const askQuestion = async (question: string) => {
+  const res = await fetch(
+    new Request(createURL('/api/question'), {
+      method: 'POST',
+      body: JSON.stringify({ question }),
+    })
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    console.log(data, 'data');
+    console.log(data.data, 'data.data');
+    return data.data;
+  }
+};
