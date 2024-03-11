2f3e4b843402b171b4ecd884faff07748ca5463b
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-11 14:08:57 +0100

    add types

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,16 @@
-// TODO: Fix "any type" ts error
+import Header from '@/components/dashboard/Header';
+import Sidebar from '@/components/dashboard/Sidebar';
 
-import Header from "@/components/dashboard/Header";
-import Sidebar from "@/components/dashboard/Sidebar";
-
-const DashboardLayout = ({ children }) => {
-    return <div className="flex flex-row h-screen w-screen">
-        <Sidebar />
-        <div className="w-full h-full flex flex-col flex-1">
-            <Header />
-            {children}
-        </div>
-        
-    </div>;
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-row h-screen w-screen">
+      <Sidebar />
+      <div className="w-full h-full flex flex-col flex-1">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
