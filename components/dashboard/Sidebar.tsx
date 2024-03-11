64913460adb410eea7fbd03e50a69bfd1dcecabe
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-11 14:15:53 +0100

    feat: add navigation to sidebar

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+];
+
 const Sidebar = () => {
-    return (
-        <aside className="h-full w-[14%] min-w-48 border-r border-black/20 bg-red-500">
-            Mood            
-        </aside>
-    );
-}
+  return (
+    <aside className="h-full w-[14%] min-w-48 border-r border-gray-500/20">
+      <div>Devjournal</div>
+          <ul>
+              {links.map(link => (
+              <li key={link.label} className="px-2 py-6 text-xl">
+                  <Link href={link.href}>{link.label}</Link>
+          </li>
+      ))}</ul>
+    </aside>
+  );
+};
 
 export default Sidebar;
