38a3feeb2d28cb4f4afcd1863d203fe126e40987
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-11 14:14:14 +0100

    feat: add routing for question

---
+++ app/api/question/route.ts
@@ -0,0 +1,25 @@
+import { qa } from '@/utils/ai';
+import { getUserByClerkId } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+import { NextResponse } from 'next/server';
+
+export const POST = async (request: any) => {
+  console.log('post question')
+  const { question } = await request.json();
+  const user = await getUserByClerkId({});
+
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+    },
+  });
+
+  const answer = await qa(question, entries);
+  console.log(answer);
+  return NextResponse.json({ data: answer });
+};
