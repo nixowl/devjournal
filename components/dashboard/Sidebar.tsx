bfba6f489d557a948a898dcd641c2fed837e8a02
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-03-12 12:21:48 +0100

    add history to nav

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 const links = [
   { href: '/', label: 'Home' },
   { href: '/journal', label: 'Journal' },
+  { href: '/history', label: 'History' },
 ];
 
 const Sidebar = () => {
